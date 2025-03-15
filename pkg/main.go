037a644c7a82f1a79e2d09f683d033707ffe0553
PapaDjo2000 <maksimdadychyn@gmail.com>
2025-03-15 19:34:57 +0300

    /

--- pkg/main.go
+++ pkg/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -16,18 +17,70 @@ type UserRequest struct {
 	Backload           int     // обратные тонны
 }
 
-func NewUserRequest(Consumption, FuelResidue float64, Capacity, SpeedometerResidue, Refuel, Distance, QuantityTrips, Tons, Backload int) *UserRequest {
-	return &UserRequest{
-		Consumption:        Consumption,
-		Capacity:           Capacity,
-		FuelResidue:        FuelResidue,
-		SpeedometerResidue: SpeedometerResidue,
-		Refuel:             Refuel,
-		Distance:           Distance,
-		QuantityTrips:      QuantityTrips,
-		Tons:               Tons,
-		Backload:           Backload,
+func (ur *UserRequest) SetConsumption(cons float64) (float64, error) {
+	if cons <= 0 {
+		return 0, fmt.Errorf("invalid consumption value")
 	}
+	ur.Consumption = cons
+	return ur.Consumption, nil
+}
+func (ur *UserRequest) SetCapacity(cap int) (int, error) {
+	if cap <= 0 {
+		return 0, fmt.Errorf("invalid consumption value")
+	}
+	ur.Capacity = cap
+	return ur.Capacity, nil
+}
+func (ur *UserRequest) SetFuelResidue(fr float64) (float64, error) {
+	if fr < 0 {
+		return 0, fmt.Errorf("invalid consumption value")
+	}
+	ur.FuelResidue = fr
+	return ur.FuelResidue, nil
+}
+func (ur *UserRequest) SetSpeedometerResidue(spr int) (int, error) {
+	if spr < 0 {
+		return 0, fmt.Errorf("invalid consumption value")
+	}
+	ur.SpeedometerResidue = spr
+	return ur.SpeedometerResidue, nil
+}
+
+func (ur *UserRequest) SetRefuel(ref int) (int, error) {
+	if ref < 0 {
+		return 0, fmt.Errorf("invalid consumption value")
+	}
+	ur.Refuel = ref
+	return ur.Refuel, nil
+}
+
+func (ur *UserRequest) SetDistance(dis int) (int, error) {
+	if dis < 0 {
+		return 0, fmt.Errorf("invalid consumption value")
+	}
+	ur.Distance = dis
+	return ur.Distance, nil
+}
+func (ur *UserRequest) SetQuantityTrips(qt int) (int, error) {
+	if qt <= 0 {
+		return 0, fmt.Errorf("invalid consumption value")
+	}
+	ur.QuantityTrips = qt
+	return ur.QuantityTrips, nil
+}
+func (ur *UserRequest) SetTons(ton int) (int, error) {
+	if ton <= 0 {
+		return 0, fmt.Errorf("invalid consumption value")
+	}
+	ur.Tons = ton
+	return ur.Tons, nil
+}
+func (ur *UserRequest) SetBackload(dton int) (int, error) {
+	if dton < 0 {
+		return 0, fmt.Errorf("invalid consumption value")
+	}
+	ur.Backload = dton
+	return ur.Backload, nil
 }
 
 func (ur *UserRequest) Сalculations() (int, float64) {
